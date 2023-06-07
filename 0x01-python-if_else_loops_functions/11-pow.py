cef704db6eaf18bec66d293ba60a859b8ca3570c
Amilemia <aminemadah2@gmail.com>
2023-06-07 01:15:26 +0100

    Task 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
 
-def add(a, b):
-    return a ** b
+def pow(a, b):
+    result = 1
+    if b >= 0:
+        for _ in range(b):
+            result *= a
+    else:
+        for _ in range(abs(b)):
+            result /= a
+    return result
