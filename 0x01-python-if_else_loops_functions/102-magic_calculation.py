6afdcb549d9304d8ee55cf354f95eeeebbb9f6e4
Amilemia <aminemadah2@gmail.com>
2023-06-07 02:09:49 +0100

    Task 16

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -3,9 +3,8 @@
 
 def magic_calculation(a, b, c):
     if a < b:
-        if c > b:
-            return c
-        else:
-            return a + b
+        return c
+    elif c > b:
+        return a + b
     else:
         return a * b - c
