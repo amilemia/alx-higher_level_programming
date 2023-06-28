03396341f480fd7af4c284e3cc6535a8baeb8db2
Amilemia <aminemadah2@gmail.com>
2023-06-28 03:54:50 +0100

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a Square class"""
+
+
+class Square:
+    """Represent a square object"""
+
+    def __init__(self, size):
+        """Initialize a new Square
+
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
