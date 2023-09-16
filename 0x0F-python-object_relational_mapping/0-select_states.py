a9db2e8b2e0e60f2f8d94e700d9861a29b398877
Amilemia <aminemadah2@gmail.com>
2023-09-16 06:41:02 +0100

    Fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-select_states
+Script that lists all states
+from the database hbtn_0e_0_usa.
 """
 if __name__ == "__main__":
     import MySQLdb
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 """
-select_states
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa.
 """
+
 if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    db_host = "localhost"
-    db_user = sys.argv[1]  # "your_username"
-    db_password = sys.argv[2]  # "your_password"
-    db_name = sys.argv[3]  # "your_database_name"
-    port = 3306
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
 
     db = MySQLdb.connect(
-        host=db_host, user=db_user, passwd=db_password, db=db_name, port=port
+        host="localhost",
+        user=db_user,
+        passwd=db_password,
+        db=db_name,
+        port=3306
     )
+
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name \
-LIKE BINARY 'N%' ORDER BY id ASC")
-    rows = cursor.fetchall()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
 
