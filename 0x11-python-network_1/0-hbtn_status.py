cb2ab9fe22e75e32246e765aff31b882c90816d5
Amilemia <aminemadah2@gmail.com>
2023-09-29 23:45:58 +0100

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,32 +1,13 @@
 #!/usr/bin/python3
 """
 Fetches the status of the ALX intranet website.
-
-Usage:
-    python 0-hbtn_status.py
-
-Output:
-    Body response:
-        - type: <class 'bytes'>
-        - content: b'OK'
-        - utf8 content: OK
 """
 
-import urllib.request
+from urllib.request import urlopen
 
-# Open the URL in a with statement
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-    # Get the body of the response
-    body = response.read()
-
-# Display the body of the response
-print("Body response:")
-
-# Print the type of the body
-print("- type:", type(body))
-
-# Print the content of the body
-print("- content:", body)
-
-# Print the utf-8 content of the body
-print("- utf8 content:", body.decode())
+with urlopen("https://alx-intranet.hbtn.io/status") as response:
+    html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode("utf-8")))
