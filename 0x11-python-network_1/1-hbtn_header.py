0ae779a2104ed127c12e3f1c8585a31f1888ecaa
Amilemia <aminemadah2@gmail.com>
2023-09-29 23:46:03 +0100

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,36 +1,12 @@
 #!/usr/bin/python3
 """
-    Sends a request to given URL & returns value of the X-Request-Id header.
+Sends a request to given URL & returns value of the X-Request-Id header.
+"""
 
-    Args:
-        url: The URL to send the request to.
+if __name__ == "__main__":
+    from urllib.request import urlopen
+    from sys import argv
 
-    Returns:
-        The value of the X-Request-Id header.
-    """
-
-import urllib.request
-import sys
-
-
-def get_request_id(url):
-
-    # Open the URL in a with statement
-    with urllib.request.urlopen(url) as response:
-        # Get the headers of the response
-        headers = response.headers
-
-        # Get the value of the X-Request-Id header
-        request_id = headers.get("X-Request-Id")
-
-    return request_id
-
-
-# Get the URL from the command line arguments
-url = sys.argv[1]
-
-# Get the request ID
-request_id = get_request_id(url)
-
-# Display the request ID
-print(request_id)
+    with urlopen(argv[1]) as response:
+        head = response.headers.get("X-Request-Id")
+        print(head)
